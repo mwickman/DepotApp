08047a24c85f06c363b5a3b89a4901e772f613a8
Matthew Wickman <wickman.matthew@gmail.com>
2012-06-27 14:14:30 -0400

    Been awhile since last commit, added some untracked files

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,26 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "depot@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    #@greeting = "Hi"
+    @order = order
+    
+    mail to: order.email, subject: "Pragmatic Store Order Confirmation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: "Pragmatic Store Order Shipped"
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+#ActionMailer configuration
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :test
+end
